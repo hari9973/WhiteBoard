0eee215686b64839bd3acff068402043724c0708
Hari Prasad Pothuri <pothurihariprasad9973@gmail.com>
2023-02-08 14:40:44 +0000

    Initial Commit

---
+++ WhiteBoardApp/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+exports.__esModule = true;
+
+var _Whiteboard = _interopRequireDefault(require("./components/Whiteboard"));
+
+exports.Whiteboard = _Whiteboard.default;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
